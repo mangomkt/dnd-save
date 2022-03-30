87795438711da1de47adc32f600302cb911bc1c7
mangomkt <cg@mangomkt.com>
2022-03-30 10:42:37 -0600

    Start

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,132 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <div class="row">
+      <button @click="save">Save</button>
+    </div>
+    <div class="row">
+      <div class="sidebar">
+        <h3>Items</h3>
+        <draggable
+          class="dragArea list-group"
+          :list="modules"
+          :group="{ name: 'people', pull: 'clone', put: false }"
+          :clone="cloneItem"
+          @change="log"
+        >
+          <div
+            class="list-group-item"
+            v-for="element in modules"
+            :key="element.name"
+          >
+            {{ element.name }}
+          </div>
+        </draggable>
+        <rawDisplayer class="col-3" :value="pagecontent" title="Content JSON" />
+      </div>
+
+      <div class="content">
+        <h3>Content</h3>
+        <draggable
+          class="dragArea list-group"
+          :list="pagecontent"
+          group="people"
+          @change="log"
+        >
+          <div
+            class="list-group-item"
+            v-for="(element, idx) in pagecontent"
+            :key="idx"
+          >
+            <div class="card-title">{{ element.name }}</div>
+            <input type="text" v-if="element.type === 'title'" v-model="element.text" />
+            <textarea v-if="element.type === 'textarea'" v-model="element.text" ></textarea>
+            <select v-if="element.type === 'iconcard'" v-model="element.cols">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <div class="icon-wrap" :class="'col-' + [element.cols]">
+              <div
+                class="icon-items"
+                v-for="(it, cidx) in element.items"
+                :key="cidx"
+              > 
+                <div class="icon-items-inner">
+                  <label>Font Awesome Class</label>
+                  <input type="text" v-model="it.icon" />
+                  <label>Descriptive Text</label>
+                  <textarea type="text" v-model="it.text" ></textarea>
+                  <i class="fa fa-times item-del" @click="removeChildAt(idx,cidx)"></i>
+                </div>
+              </div>
+            </div>
+            <button v-if="element.items" class="" @click="add(idx)" >Add</button>
+            <i class="fa fa-times item-del" @click="removeAt(idx)"></i>
+          </div>
+        </draggable>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import draggable from "vuedraggable";
 export default {
-  name: 'App',
+  name: "clone",
+  display: "Clone",
+  order: 2,
   components: {
-    HelloWorld
-  }
-}
-</script>
+    draggable
+  },
+  data() {
+    return {
+      modules: [
+        { name: "Title", type: "title" },
+        { name: "Textarea", type: "textarea" },
+        { name: "Icon Card", type: "iconcard" }
+      ],
+      pagecontent: JSON.parse(localStorage.content)
+    };
+  },
+  mounted: {
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  },
+  methods: {
+    log: function(evt) {
+      window.console.log(evt);
+    },
+    removeAt: function(idx) {
+      this.pagecontent.splice(idx, 1);
+    },
+    removeChildAt: function(idx, cidx) {
+      this.pagecontent[idx].items.splice(cidx, 1);
+    },
+    add: function(idx) {
+      this.pagecontent[idx].items.push({icon: "fa", text: "foo"});
+    },
+    cloneItem({name, type }) {
+
+      if(type === "iconcard") {
+        return {
+          name: `${name}`,
+          type: `${type}`,
+          cols: "3",
+          items: [
+            {icon: "fa fa-link", text: "foo bar"}
+          ]
+        };
+      } else {
+        return {
+          name: `${name}`,
+          type: `${type}`,
+        };
+      }
+    },
+    save: function() {
+      localStorage.content = JSON.stringify(this.pagecontent);
+    },
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import rawDisplayer from "./components/rawDisplayer.vue";
+import "font-awesome/css/font-awesome.css";
+import "./css/main.css";
 
 Vue.config.productionTip = false
-
+Vue.component("rawDisplayer", rawDisplayer);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
